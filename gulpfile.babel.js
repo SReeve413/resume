2627c0bef06728a3337cc960aa0df9cf570b923b
Unknown <Shanereeve@gmail.com>
2018-02-16 12:24:55 -0700

    updating files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -68,39 +68,39 @@ gulp.task('copy', () =>
     .pipe($.size({title: 'copy'}))
 );
 
-// // Compile and automatically prefix stylesheets
-// gulp.task('styles', () => {
-//   const AUTOPREFIXER_BROWSERS = [
-//     'ie >= 10',
-//     'ie_mob >= 10',
-//     'ff >= 30',
-//     'chrome >= 34',
-//     'safari >= 7',
-//     'opera >= 23',
-//     'ios >= 7',
-//     'android >= 4.4',
-//     'bb >= 10'
-//   ];
-//
-//   // For best performance, don't add Sass partials to `gulp.src`
-//   return gulp.src([
-//     'app/styles/**/*.scss',
-//     'app/styles/**/*.css'
-//   ])
-//     .pipe($.newer('.tmp/styles'))
-//     .pipe($.sourcemaps.init())
-//     .pipe($.sass({
-//       precision: 10
-//     }).on('error', $.sass.logError))
-//     .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
-//     .pipe(gulp.dest('.tmp/styles'))
-//     // Concatenate and minify styles
-//     .pipe($.if('*.css', $.cssnano()))
-//     .pipe($.size({title: 'styles'}))
-//     .pipe($.sourcemaps.write('./'))
-//     .pipe(gulp.dest('dist/styles'))
-//     .pipe(gulp.dest('.tmp/styles'));
-// });
+// Compile and automatically prefix stylesheets
+gulp.task('styles', () => {
+  const AUTOPREFIXER_BROWSERS = [
+    'ie >= 10',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4.4',
+    'bb >= 10'
+  ];
+
+  // For best performance, don't add Sass partials to `gulp.src`
+  return gulp.src([
+    'app/styles/**/*.scss',
+    'app/styles/**/*.css'
+  ])
+    .pipe($.newer('.tmp/styles'))
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      precision: 10
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+    .pipe(gulp.dest('.tmp/styles'))
+    // Concatenate and minify styles
+    .pipe($.if('*.css', $.cssnano()))
+    .pipe($.size({title: 'styles'}))
+    .pipe($.sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/styles'))
+    .pipe(gulp.dest('.tmp/styles'));
+});
 
 // Concatenate and minify JavaScript. Optionally transpiles ES2015 code to ES5.
 // to enable ES2015 support remove the line `"only": "gulpfile.babel.js",` in the
